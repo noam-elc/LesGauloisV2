8be6063ab38dbd85e9b22e834eadb2b97ba80387
noam-elc <carrion.noam@gmail.com>
2026-03-16 16:45:46 +0100

    Question 1.1

---
+++ src/test_fonctionnel/TestGaulois.java
@@ -0,0 +1,11 @@
+package test_fonctionnel;
+
+import personnages.Gaulois;
+
+public class TestGaulois {
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		Gaulois obelix = new Gaulois("Obélix", 16);
+		
+	}
+}
